fb177e0de75303976ec54805a64a807fe2c25c7b
Ross Blanchard <blanchard.ross@gmail.com>
2018-08-05 22:21:03 -0700

    using cargo

---
+++ chapter_1/hello_cargo/target/debug/deps/hello_cargo-070cb38eb05ee964.d
@@ -0,0 +1,5 @@
+/Users/rossi/rust_projects/TRPL_exercises/chapter_1/hello_cargo/target/debug/deps/hello_cargo-070cb38eb05ee964: src/main.rs
+
+/Users/rossi/rust_projects/TRPL_exercises/chapter_1/hello_cargo/target/debug/deps/hello_cargo-070cb38eb05ee964.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/rossi/rust_projects/TRPL_exercises/chapter_1/hello_cargo/target/debug/hello_cargo.dSYM: /Users/rossi/rust_projects/TRPL_exercises/chapter_1/hello_cargo/src/main.rs
